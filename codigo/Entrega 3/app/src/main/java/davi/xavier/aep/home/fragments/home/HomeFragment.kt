b328db19ae53dbcc1a1a11e1e1e6a1601aafbb6f
davi_xavier <47609623+davirxavier@users.noreply.github.com>
2021-08-10 22:14:41 -0300

    [entrega4] - Atualizações de localização

--- codigo/Entrega 3/app/src/main/java/davi/xavier/aep/home/fragments/home/HomeFragment.kt
+++ codigo/Entrega 3/app/src/main/java/davi/xavier/aep/home/fragments/home/HomeFragment.kt
@@ -1,11 +1,26 @@
 package davi.xavier.aep.home.fragments.home
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
@@ -19,15 +34,19 @@ import davi.xavier.aep.data.UserViewModel
 import davi.xavier.aep.databinding.FragmentHomeBinding
 import kotlinx.coroutines.launch
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), SensorEventListener, LocationListener {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var mapFrag: SupportMapFragment
+    private lateinit var locationManager: LocationManager
+    private lateinit var sensorManager: SensorManager
+    private lateinit var permissionRequest: ActivityResultLauncher<String>
+    private var sensorStep: Sensor? = null
     private val statsViewModel: StatsViewModel by activityViewModels { 
         StatsViewModel.StatsViewModelFactory(
             (activity?.application as AepApplication).statRepository
         )
     }
-    private val userViewModel: UserViewModel by activityViewModels() {
+    private val userViewModel: UserViewModel by activityViewModels {
         UserViewModel.AuthViewModelFactory(
             (activity?.application as AepApplication).userRepository
         )
@@ -35,6 +54,35 @@ class HomeFragment : Fragment() {
     
     private var isProcessing = true
     private var isPlaying = false
+    private var currentSavedPoints: MutableList<LatLng> = mutableListOf()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        permissionRequest = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) {
+                requestLocations()
+            } else {
+                // TODO Mensagem dizendo que a funcionalidade está desabilitada
+            }
+        }
+
+        sensorManager = requireContext().getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorStep = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    }
+    
+    @SuppressLint("MissingPermission")
+    private fun requestLocations() {
+        val perm = askForLocationPermission()
+        
+        if (perm) {
+            locationManager.getBestProvider(Criteria(), true)?.let {
+                locationManager.requestLocationUpdates(it, 5000, 0f, this)
+            }
+        }
+    }
     
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,7 +95,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        
         mapFrag = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFrag.onCreate(savedInstanceState)
         mapFrag.getMapAsync { mapCallback(it) }
@@ -63,10 +111,18 @@ class HomeFragment : Fragment() {
             
             val drawable = if (isPlaying) R.drawable.ic_baseline_stop_24 else R.drawable.ic_baseline_play_arrow_24
             binding.startButton.setImageDrawable(resources.getDrawable(drawable, requireContext().theme))
+            
+            if (isPlaying) {
+                requestLocations()
+            } else {
+                currentSavedPoints = mutableListOf()
+            }
         })
     }
     
     private fun onPlayClick() {
+        if (!askForLocationPermission()) return
+        
         if (!isProcessing) {
             lifecycleScope.launch {
                 isProcessing = true
@@ -76,6 +132,8 @@ class HomeFragment : Fragment() {
                     try {
                         statsViewModel.finishStats()
                         userViewModel.setCurrentStatUid(null)
+                        
+                        locationManager.removeUpdates(this@HomeFragment)
                     } catch (e: Exception) {
                         Log.e("CREATE_STAT_ERROR", e.message ?: "", e)
                         message = R.string.unknown_error
@@ -104,5 +162,40 @@ class HomeFragment : Fragment() {
                 .title("UFC - Campus Quixadá")
         )
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(quix, 15f))
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.takeIf { sensorStep != null && event.sensor == sensorStep }?.let {
+            Log.e("Steps", event.values[0].toString())
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+    
+    private fun askForLocationPermission(): Boolean {
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+               return true
+            }
+            
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                // TODO Mensagem explicando a utilização da funcionalidade
+                return false
+            }
+            else -> {
+                permissionRequest.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                return false
+            }
+        }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        val last = currentSavedPoints.lastOrNull()
+        if (last == null || last.latitude != location.latitude || last.longitude != location.longitude) {
+            currentSavedPoints.add(LatLng(location.latitude, location.longitude))
+        }
     }
 }
--- codigo/Entrega 3/app/src/main/java/davi/xavier/aep/util/Constants.kt
+++ codigo/Entrega 3/app/src/main/java/davi/xavier/aep/util/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
     const val USER_INFO_PATH = "user_info"
     const val STATS_PATH = "stats"
     const val CURRENT_STAT = "current_stat"
+    
+    const val REQUEST_CODE_LOCATION = 0
 }
